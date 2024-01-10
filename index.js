cb48d6019d53c04fba347ff0df0cdac28e461a78
Andrew Ahn <andrewahn21@gmail.com>
2024-01-09 17:57:57 -0800

    Established a connection with the database.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = require('./routes/mainRoutes')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
@@ -9,8 +11,17 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+
+
+
 app.use('/api/study-sessions', router)
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
