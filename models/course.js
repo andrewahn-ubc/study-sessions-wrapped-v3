fc6890ad1c20700e9a906d18708596123c3a97c7
Andrew Ahn <andrewahn21@gmail.com>
2024-01-09 18:20:35 -0800

    Created a new Schema to model a course.

--- models/course.js
+++ models/course.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const courseSchema = new Schema({
+    courseCode: {
+        type: String,
+        required: true
+    },
+    courseNumber: {
+        type: Number,
+        required: true
+    },
+    courseDesc: {
+        type: String,
+        required: true
+    },
+    studyTimeSoFar: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+// Creates the model right before exporting.
+module.exports = mongoose.model("Course", courseSchema)
