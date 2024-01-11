9bf3b21c54fc11c568b5175901fac12f62f1d812
Andrew Ahn <andrewahn21@gmail.com>
2024-01-10 23:02:12 -0800

    Ensured that all routes in mainRoutes work.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,23 +1,24 @@
+const Course = require('./models/course')
+const mongoose = require('mongoose')
 
-
-const getAllCourses = () => {
-    console.log('hi')
+const getAllCourses = (req, res) => {
+    res.status(200).json({mssg: "get all courses"})
 }
 
-const getCourse = () => {
-    console.log('hi')
+const getCourse = (req, res) => {
+    res.status(200).json({mssg: "get a single course"})
 }
 
-const createCourse = () => {
-    console.log('hi')
+const createCourse = (req, res) => {
+    res.status(200).json({mssg: "create a course"})
 }
 
-const updateCourse = () => {
-    console.log('hi')
+const updateCourse = (req, res) => {
+    res.status(200).json({mssg: "update a course"})
 }
 
-const deleteCourse = () => {
-    console.log('hi')
+const deleteCourse = (req, res) => {
+    res.status(200).json({mssg: "delete a course"})
 }
 
 module.exports = {
