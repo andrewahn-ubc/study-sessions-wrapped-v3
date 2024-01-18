fea88ccdd5326cfbc411a7d4ed85883eff75884b
Andrew Ahn <andrewahn21@gmail.com>
2024-01-18 01:50:52 -0800

    "get all courses" functionality working.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+    const [courses, setCourses] = useState(null)
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const response = await fetch('/api/study-sessions')
+            const json = await response.json()
+
+            if (response.ok) {
+                setCourses(json)
+            }
+        }
+
+        fetchCourses()
+    }, [])
+
     return (
         <div className='Home'>
-            <h1>Home</h1>
+            <div className="courses">
+                {/* why do we use parenthesis in the return statement in the map function? */}
+                {courses && courses.map((course) => (
+                    <p key={course._id}>{course.courseCode} </p>
+                ))}
+            </div>
         </div>
     )
 }
