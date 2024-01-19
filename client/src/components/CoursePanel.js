bc616d4d929ad792fed311bf16d8496af99d4ebb
Andrew Ahn <andrewahn21@gmail.com>
2024-01-18 16:57:34 -0800

    Implemented "delete course".

--- client/src/components/CoursePanel.js
+++ client/src/components/CoursePanel.js
@@ -1,11 +1,22 @@
 import './CoursePanel.css'
 
 const CoursePanel = ({course}) => {
+    const deleteCourse = async () => {
+        const response = await fetch("/api/study-sessions/" + course._id, {
+            method: "DELETE"
+        })
+
+        if (!response.ok) {
+            console.log("Error occurred while deleting")
+        }
+    }
+
     return (
         <div className='coursePanel'>
             <h1 className='courseTitle' >{course.courseCode + " " + course.courseNumber}</h1>
-            <p className='courseDesc' >{course.courseDesc} </p>
+            <p className='courseDesc' ><strong>{course.courseDesc}</strong></p>
             <p className='studyTime' >Time Studied: {course.studyTimeSoFar} </p>
+            <span className="deleteButton" onClick={deleteCourse}>Delete</span>
         </div>
     )
 }
