1da979ddfb3df114cfb5306a5c92e553afb534d4
Andrew Ahn <andrewahn21@gmail.com>
2024-01-22 16:51:41 -0800

    Implemented most of the reducer functions.

--- client/src/context/CoursesContextProvider.js
+++ client/src/context/CoursesContextProvider.js
@@ -2,8 +2,21 @@ import { createContext } from "react"
 
 export const CoursesContext = createContext()
 
+export const coursesReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET-COURSES':
+            return (action.payload)
+        case 'ADD-COURSE':
+            return [action.payload, ...state]
+        case 'DELETE-COURSE':
+            return (state.filter((course) => {course._id !== action.payload._id}))
+        default:
+            return state
+    }
+}
+
 export const CoursesContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer({})
+    const [state, dispatch] = useReducer(coursesReducer, {})
 
     return (
         <CoursesContext.Provider value={{state, dispatch}}>
